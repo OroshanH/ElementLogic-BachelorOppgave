f6b83948022edc0eb80b672c85071a937d917c71
Orosh <Oroshanh@hotmail.com>
2023-02-22 17:41:28 +0100

    Fikset på stock page. Lister nå nødvendig data fra begge tabellene.

--- src/main/resources/static/stockpage/stock.js
+++ src/main/resources/static/stockpage/stock.js
@@ -3,20 +3,21 @@ $(function(){
 });
 
 function hentAlleInbound() {
-    $.get( "/hentAlleInbound", function( data ) {
-        formaterData(data);
+  $.get( "/hentAlleInbound", function( inboundData ) {
+    $.get( "/hentAlle", function( produkterData ) {
+      formaterData(inboundData, produkterData);
     });
-};
+  });
+}
 
 
-
-function formaterData(inboundProdukter){
+function formaterData(inboundData , produkterData){
     var ut = "<table class='table table-striped'>" +
         "<tr>" +
         "<th>ProduktID</th><th>Navn</th><th>Beskrivelse</th><th>Antall</th>" +
         "</tr>";
-    for(let i in inboundProdukter ){
-        ut+="<tr><td>"+inboundProdukter[i].produktid+"</td><td>"+"N/A"+"</td><td>"+"N/A"+"</td><td>" + inboundProdukter[i].quantity + "</td> </tr>"
+    for(let i in inboundData ){
+        ut+="<tr><td>"+ inboundData[i].produktid + "</td><td>"+ produkterData[i].navn +"</td><td>"+ produkterData[i].beskrivelse +"</td><td>" + inboundData[i].quantity + "</td> </tr>"
     }
     $("#produktene").html(ut);
 }
