6ec2de356286a6eb3700624654b6baa110747ab2
Orosh <Oroshanh@hotmail.com>
2023-03-05 14:49:46 +0100

    tester

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO Produkt (produktid, navn, beskrivelse) VALUES ( '3','Joggesko','Svarte joggesko,størrelse 42' );
-INSERT INTO Produkt (produktid, navn, beskrivelse) VALUES ( '5','Genser','Hvit genser, størrelse M' );
+INSERT INTO PRODUKT (produktid, navn, beskrivelse) VALUES ( '3','Joggesko','Svarte joggesko,størrelse 42' );
+INSERT INTO PRODUKT (produktid, navn, beskrivelse) VALUES ( '5','Genser','Hvit genser, størrelse M' );
 
 
 
